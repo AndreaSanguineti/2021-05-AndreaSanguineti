a087a081b20e4c413dae9407a79e8cfbc6ca3c03
AndreaSanguineti <andrealaura.sanguineti@mail.polimi.it>
2021-12-13 16:52:26 +0100

    push04

--- public/sketch.js
+++ public/sketch.js
@@ -5,10 +5,32 @@ function setup() {
   background(51);
 
   socket = io.connect("http://localhost:3000");
+  socket.on("mouse", newDrawing);
 }
 
 function draw() {
   noStroke();
   fill(255);
-  ellipse(mouseX, mouseY, 60, 60);
+  ellipse(mouseX, mouseY, 36, 36);
+}
+
+function newDrawing(data) {
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 36, 36);
+}
+
+function mouseDragged() {
+  console.log(mouseX + "," + mouseY);
+
+  var data = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  socket.emit("mouse", data);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 36, 36);
 }
